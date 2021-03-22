e5a26bdeaa4d073b73bee6a1fcbd445fc995dcab
SrtaK <atorrecillam@gmail.com>
2021-03-22 13:41:08 +0100

    probando las rutas

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -16,12 +16,28 @@ function pruebas(req, res){
     });
 }
 
+function pruebas2(req, res){
+    console.log(req.body);
+    res.status(200).send({
+        message: 'Hello, you are testing the app 2'
+    });
+}
+
+function cuartoNivel(req, res){
+    console.log(req.body);
+    res.status(200).send({
+        message: 'Hello, you are in the fourth level'
+    });
+}
+
+
 //exportar en forma de objeto, 
 //así están dispoblies fuera de este fichero
-
 module.exports = {
     home, 
-    pruebas
+    pruebas,
+    pruebas2,
+    cuartoNivel
 }
 
 
--- api/routes/user.js
+++ api/routes/user.js
@@ -9,7 +9,9 @@ var UserController = require('../controllers/user');
 var api = express.Router(); 
 
 //routes
-//api.get('/home', UserController.home);
+api.get('/home', UserController.home);
 api.get('/pruebas', UserController.pruebas);
+api.post('/pruebas2', UserController.pruebas2);
+api.get('/uno/dos/tres/cuartoNivel', UserController.cuartoNivel);
 
 module.exports = api;
